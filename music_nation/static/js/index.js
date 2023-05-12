3235d679bc4e34cb1904583da412c4d29178fd7c
Asif Biswas <asifbiswas85@gmail.com>
2023-05-13 00:49:27 +0600

    done

--- music_nation/static/js/index.js
+++ music_nation/static/js/index.js
@@ -22,79 +22,6 @@ const progressBar = document.getElementById("progress-bar");
 let currentTrack = 0;
 let currentList;
 
-// let tracks = [
-//   {
-//     track: 1,
-//     name: "Tones And I",
-//     artist: "Dance Monkey",
-//     duration: "03:56",
-//     image:
-//       "https://is3-ssl.mzstatic.com/image/thumb/Music123/v4/85/8e/3e/858e3ef9-cf9e-e54c-fb73-b2513694a308/075679838872.jpg/268x0w.jpg",
-//     url:
-//       "https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview123/v4/22/e5/8e/22e58eeb-5916-c584-257c-735e1b4b0175/mzaf_17966999117302129087.plus.aac.p.m4a"
-//   },
-//   {
-//     track: 2,
-//     name: "Never Never",
-//     artist: "Drenchill",
-//     duration: "02:47",
-//     image:
-//       "https://is4-ssl.mzstatic.com/image/thumb/Music113/v4/cc/99/b4/cc99b464-414c-a097-d896-11e0c371308a/886447895276.jpg/626x0w.jpg",
-//     url:
-//       "https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview123/v4/28/53/c1/2853c193-df5f-72cb-ea6a-062b7ec432af/mzaf_10693040049667551718.plus.aac.p.m4a"
-//   },
-//   {
-//     track: 3,
-//     name: "Falling",
-//     artist: "Trevor Daniel",
-//     duration: "02:39",
-//     image:
-//       "https://i.pinimg.com/736x/5c/27/fb/5c27fb8c0b480d7ce08b15f57cbe53ff.jpg",
-//     url:
-//       "https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview128/v4/83/bc/70/83bc709e-25de-3797-8450-c751df462713/mzaf_7811558218024776548.plus.aac.p.m4a"
-//   },
-//   {
-//     track: 4,
-//     name: "Ride It",
-//     artist: "Regard",
-//     duration: "02:20",
-//     image:
-//       "https://is2-ssl.mzstatic.com/image/thumb/Music123/v4/61/bf/01/61bf0139-35c7-8f06-432d-9e7d316fba32/886447846674.jpg/626x0w.jpg",
-//     url:
-//       "https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview123/v4/0a/82/fe/0a82feaf-ab78-c268-d91d-65f698b91ce1/mzaf_3492393954382629146.plus.aac.p.m4a"
-//   },
-//   {
-//     track: 5,
-//     name: "everything i wanted",
-//     artist: "Billie Eilish",
-//     duration: "04:47",
-//     image:
-//       "https://is3-ssl.mzstatic.com/image/thumb/Music123/v4/85/57/85/855785ff-6433-a6b5-45e2-ccee55d608dc/19UM1IM00404.rgb.jpg/626x0w.jpg",
-//     url:
-//       "https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview123/v4/72/cf/5a/72cf5ad7-e1b3-d6ce-84b1-973d0b52d10b/mzaf_2144917941139718442.plus.aac.p.m4a"
-//   },
-//   {
-//     track: 6,
-//     name: "Don't Start Now",
-//     artist: "Dua Lipa",
-//     duration: "03:01",
-//     image:
-//       "https://is2-ssl.mzstatic.com/image/thumb/Music123/v4/58/89/69/58896934-fedf-e9ca-494f-48c06d6a259a/190295322175.jpg/626x0w.jpg",
-//     url:
-//       "https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview123/v4/d0/0b/d2/d00bd29b-1a4a-239b-23df-b3df6bb7d30e/mzaf_16326686631763178212.plus.aac.p.m4a"
-//   },
-
-//   {
-//     track: 7,
-//     name: "Nightmare",
-//     artist: "Halsey",
-//     duration: "04:47",
-//     image:
-//       "https://is5-ssl.mzstatic.com/image/thumb/Music113/v4/96/2d/77/962d7757-a2e7-9461-d5e9-b0f724ea999f/19UMGIM38270.rgb.jpg/268x0w.jpg",
-//     url:
-//       "https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview123/v4/c8/cc/64/c8cc640a-6ae6-9f28-def2-05588c7f43dc/mzaf_520533846207392561.plus.aac.p.m4a"
-//   }
-// ];
 
 function init() {
   if (currentTrack === 0) {
@@ -232,4 +159,23 @@ music.addEventListener("ended", nextPlay, false);
 music.addEventListener("timeupdate", formatTime, false);
 music.addEventListener("timeupdate", updateProgress, false);
 
+// progressBar.addEventListener("click", function(e) {
+//   try {
+//     let percent = e.offsetX / this.offsetWidth;
+//     console.log(percent, percent * music.duration);
+//     music.currentTime = parseInt(percent * music.duration)
+//     console.log(music.duration, music.currentTime, percent * music.duration);
+//     //progressBar.setAttribute("value", percent / 100);
+//   } catch (error) {
+//     console.log(error);
+//   }
+
+// });
+
 init();
+
+
+const volume = document.getElementById("volume");
+volume.addEventListener("change", function(e) {
+  music.volume = e.currentTarget.value;
+})
